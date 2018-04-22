6f94c19c30251b5f57beae6dc3e40070e110d46e
Saleem Hadad <saleem_ha@hotmail.com>
2018-04-23 07:09:14 +0800

    gs

--- tests/Feature/PagesTest.php
+++ tests/Feature/PagesTest.php
@@ -11,6 +11,6 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertStatus(200)->assertSee('ILoveMath');
     }
 }
