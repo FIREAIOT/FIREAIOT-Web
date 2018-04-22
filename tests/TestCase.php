7c9a4608d3c64140e0b53c92072fb12cac846d92
Saleem Hadad <saleem_ha@hotmail.com>
2018-04-23 07:02:55 +0800

    generate auth scafolding

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,75 @@
 
 namespace Tests;
 
+use App\Exceptions\Handler;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, DatabaseMigrations;
+    
+    /**
+     * Called when the test is created.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        
+        DB::statement('PRAGMA foreign_keys=on;');
+        $this->disableExceptionHandling();
+    }
+    
+    /**
+     *
+     */
+    protected function disableExceptionHandling()
+    {
+        $this->oldExceptionHandler = $this->app->make(ExceptionHandler::class);
+        
+        $this->app->instance(ExceptionHandler::class, new class extends Handler {
+            public function __construct() {}
+            public function report(\Exception $e) {}
+            public function render($request, \Exception $e) {
+                throw $e;
+            }
+        });
+    }
+    
+    /**
+     * @return $this
+     */
+    protected function withExceptionHandling()
+    {
+        $this->app->instance(ExceptionHandler::class, $this->oldExceptionHandler);
+        return $this;
+    }
+    
+    /**
+     * @return array
+     */
+    public function unauthorizedJsonResponse()
+    {
+        return [
+            "data"    => [],
+            "status"  => "error",
+            "message" => "Unauthorized.",
+            "errors"  => []
+        ];
+    }
+    
+    /**
+     * @return array
+     */
+    public function unauthenticatedJsonResponse()
+    {
+        return [
+            "data"    => [],
+            "status"  => "error",
+            "message" => "Unauthenticated.",
+            "errors"  => []
+        ];
+    }
 }
