9436d5e329ddb612e31590d14643b42a278d003d
Saleem Hadad <saleem_ha@hotmail.com>
2018-05-02 20:20:36 +0800

    update UI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
         <link href="{{ asset('css/pace.css') }}" rel="stylesheet">
         <script src="{{ asset('js/pace.js') }}"></script>
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        {{--<link rel="shortcut icon" type="image/png" href="@image(fav.png)" />--}}
+        <link rel="shortcut icon" type="image/png" href="{{ asset("favicon.png") }}" />
         <!-- Fonts and icons -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
         <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700"/>
