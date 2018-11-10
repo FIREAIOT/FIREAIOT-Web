dfcc8b355bc8c05b4a03f5bebb43f79dc71501fa
Saleem Hadad <saleem_ha@hotmail.com>
2018-11-10 12:41:00 +0800

    wip

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,7 @@ Route::group(['prefix'=>'v1'], function () {
     Route::get('/users', 'UserController@index')->middleware('auth:api');
 
     Route::post('/alarms', 'AlarmController@store')->middleware('auth:api');
+    Route::put('/alarms', 'AlarmController@update')->middleware('auth:api');
 
-    Route::put('/alarms', 'AlarmController@update');
-
-    Route::put('/uav/location', 'UAVLocationController@update');
+    Route::put('/uav/location', 'UAVLocationController@update')->middleware('auth:api');
 });
